311724a87c2550c89eea462d5e73c29183596ceb
Danilo <danilo@gmail.com>
2022-03-23 10:46:12 -0300

    feat: Localstorage produtos

--- src/controllers/ControllerCart.js
+++ src/controllers/ControllerCart.js
@@ -1,35 +1,40 @@
-import { products } from "../script/script.js"
-import {Template} from "../models/Templates.js"
-let  arrayCart = []
-const ulCarrinho = document.querySelector('.list-products')
+import { products } from "../script/script.js";
+import { Template } from "../models/Templates.js";
+
+let arrayCart = [];
+
+const ulCarrinho = document.querySelector(".list-products");
 export class ControllerCart {
-    static ulCart = document.querySelector('.template-container ul')
-    static observerUl() {
-        this.ulCart.addEventListener('click', this.addCart)
+  static ulCart = document.querySelector(".template-container ul");
+  static observerUl() {
+    this.ulCart.addEventListener("click", this.addCart);
+  }
+
+  static async addCart(event) {
+    if (event.target.closest("li")) {
+      let idTarget = await Number(event.target.closest("li").id);
+
+      let cartProductsTemplate = products.find((item) => {
+        return item.id == idTarget;
+      });
+      arrayCart.push(cartProductsTemplate);
+      ControllerCart.itensLocal(cartProductsTemplate);
     }
+  }
+  static async itensLocal(item) {
+    let novosItens;
+    if (localStorage.length == 0) {
+      localStorage.setItem("usuarioProdutos", JSON.stringify(arrayCart));
+    } else if (localStorage.length > 0) {
+      novosItens = JSON.parse(localStorage.usuarioProdutos);
+      novosItens.push(item);
 
-
-
-    static async addCart(event) {
-        // console.log(products)
-        // console.log(event.target.closest('li'))
-        if (event.target.closest('li')) {
-            let idTarget = await Number(event.target.closest('li').id)
-            console.log(idTarget)
-
-            let cartProductsTemplate = products.find((item)=> {
-               return item.id == idTarget
-            })
-           arrayCart.push(cartProductsTemplate)
-        }
-
-        localStorage.setItem("usuarioProdutos", JSON.stringify(arrayCart))
-        const dataLocal = JSON.parse(localStorage.getItem("usuarioProdutos"))
-        Template.cartTemplate(dataLocal,ulCarrinho)
-        console.log(dataLocal)
+      localStorage.setItem("usuarioProdutos", JSON.stringify(novosItens));
+    } else {
     }
-
-
-
+    if (localStorage.length > 0) {
+      let x = JSON.parse(localStorage.getItem("usuarioProdutos"));
+      Template.cartTemplate(x, ulCarrinho);
+    }
+  }
 }
-
--- src/script/script.js
+++ src/script/script.js
@@ -2,11 +2,10 @@ import { Template } from "../models/Templates.js";
 import { Api } from "../api/Api.js";
 import { ControllerCart } from "../controllers/ControllerCart.js";
 
+ControllerCart.observerUl();
+export const products = await Api.getPublicProducts();
+const listatemplate = document.querySelector(".template-container ul");
 
-ControllerCart.observerUl()
-export const products = await Api.getPublicProducts()
-const listatemplate = document.querySelector('.template-container ul')
+Template.createTemplate(products, listatemplate);
 
-Template.createTemplate(products, listatemplate)
-
-console.log(JSON.parse(localStorage.getItem("usuarioProdutos")))
+// console.log(JSON.parse(localStorage.getItem("usuarioProdutos")));
