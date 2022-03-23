ca292e3253cdd78f688f2991a2a73de7ef68c217
Danilo <danilo@gmail.com>
2022-03-23 20:37:44 -0300

    fix: Pequenas correções CSS

--- src/models/Templates.js
+++ src/models/Templates.js
@@ -1,10 +1,10 @@
 export class Template {
   static async createTemplate(array, showCase) {
-    showCase.innerHTML = ""
+    showCase.innerHTML = "";
     array.forEach((item) => {
-      const { categoria, descricao, id, imagem, nome, preco } = item
-      const liProducts = document.createElement('li')
-      liProducts.setAttribute("id", id)
+      const { categoria, descricao, id, imagem, nome, preco } = item;
+      const liProducts = document.createElement("li");
+      liProducts.setAttribute("id", id);
       liProducts.innerHTML = `
       
       <img src="${imagem}" alt="img-teste" class="img-item">
@@ -18,26 +18,79 @@ export class Template {
         <img src="./src/favicons/icon_card.png" alt="img-card" class="img-card">
       </div>
       
-      `
-      showCase.appendChild(liProducts)
-    })
-
+      `;
+      showCase.appendChild(liProducts);
+    });
   }
 
   static async cartTemplate(array, showCase) {
-    showCase.innerHTML = ""
+    showCase.innerHTML = "";
     array.forEach((item) => {
-      const { imagem, nome, preco, categoria } = item
-      const liProducts = document.createElement('li')
+      const { imagem, nome, preco, categoria } = item;
+      const liProducts = document.createElement("li");
       liProducts.innerHTML = `
-      <img src="${imagem}" alt="produto no carrinho">
-      <p>${nome}</p>
-      <span>${categoria}</span>
-      <p>${preco.toFixed(2)}</p>
-      `
-      showCase.appendChild(liProducts)
-    })
+      <section class="picture-and-name">
+            <img
+              src="${imagem}"
+              alt="img-teste"
+              class="img-card"
+            />
 
+            <h2 class="title-product-card">${nome}</h2>
+
+            <img
+              src="./src/favicons/trash.png"
+              alt="img-trash"
+              class="img-trash"
+            />
+          </section>
+
+          <section class="filter-and-price">
+            <span class="filter">${categoria}</span>
+            <p class="price">R$ ${preco}</p>
+          </section>
+
+          <section class="amount-products-in-card">
+            <p class="less">-</p>
+            <p class="amount">1</p>
+            <p class="more">+</p>
+          </section>
+
+          <div class="border"></div>
+      `;
+      showCase.appendChild(liProducts);
+    });
   }
+}
+{
+  /* <li>
+          <section class="picture-and-name">
+            <img
+              src="./src/favicons/img-teste.png"
+              alt="img-teste"
+              class="img-card"
+            />
 
+            <h2 class="title-product-card">Panqueca</h2>
+
+            <img
+              src="./src/favicons/trash.png"
+              alt="img-trash"
+              class="img-trash"
+            />
+          </section>
+
+          <section class="filter-and-price">
+            <span class="filter">Padaria</span>
+            <p class="price">R$ 20,00</p>
+          </section>
+
+          <section class="amount-products-in-card">
+            <p class="less">-</p>
+            <p class="amount">1</p>
+            <p class="more">+</p>
+          </section>
+
+          <div class="border"></div>
+        </li> --> */
 }
--- src/script/script.js
+++ src/script/script.js
@@ -1,15 +1,14 @@
 import { Template } from "../models/Templates.js";
 import { Api } from "../api/Api.js";
 import { ControllerCart } from "../controllers/ControllerCart.js";
-import { ControllerFilters } from "../controllers/ControllerFilters.js"
+import { ControllerFilters } from "../controllers/ControllerFilters.js";
 ControllerCart.observerUl();
 ControllerCart.observerCart();
 ControllerCart.onload();
-let price = ControllerCart.updatePrice();
+ControllerCart.footerObserver();
 
+let price = ControllerCart.updatePrice();
 
 export const products = await Api.getPublicProducts();
 const listatemplate = document.querySelector(".template-container ul");
 Template.createTemplate(products, listatemplate);
-
-
