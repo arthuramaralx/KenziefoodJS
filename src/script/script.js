5382ed0546b8cb8392b3f84aaa34eeb65bf5caef
Arthuramaralx <Arthuramaralx@outlook.com>
2022-03-23 14:23:42 -0300

    feature: cart add, remove, price and quantity

--- src/script/script.js
+++ src/script/script.js
@@ -3,6 +3,11 @@ import { Api } from "../api/Api.js";
 import { ControllerCart } from "../controllers/ControllerCart.js";
 
 ControllerCart.observerUl();
+ControllerCart.observerCart();
+ControllerCart.onload();
+let price = ControllerCart.updatePrice();
+
+
 export const products = await Api.getPublicProducts();
 const listatemplate = document.querySelector(".template-container ul");
 
