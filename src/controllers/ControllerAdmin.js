23dae8b120cd98ddf10b7cdbec5291148e9b8cfe
Danilo <danilo@gmail.com>
2022-03-24 18:08:57 -0300

    fix: fechar janela cadastro

--- src/controllers/ControllerAdmin.js
+++ src/controllers/ControllerAdmin.js
@@ -1,50 +1,63 @@
 export class ControllerAdmin {
+  static body = document.querySelector("body");
+  static formCadastro = document.querySelector(".cadastro-produto");
+  static closeModal = document.querySelector(".section__fecharmodal");
+  static modalEdicao = document.querySelector(".modal-edicao");
+  static modalCadastro = document.querySelector(".modal__cadastroProduto");
 
-    static body         = document.querySelector("body");
-    static formCadastro = document.querySelector(".cadastro-produto");
-    static closeModal   = document.querySelector(".section__fecharmodal")
-    static modalEdicao  = document.querySelector(".modal-edicao")
-    
-    static observerModalCadastro() {
-        this.body.addEventListener("click", this.showModalCadastro)
+  static modal = document.querySelector(".sectionModal__cadastroProduto");
+
+  static observerModalCadastro() {
+    this.body.addEventListener("click", this.showModalCadastro);
+  }
+
+  static showModalCadastro(event) {
+    if (event.target.id === "novo-produto") {
+      const modal = document.querySelector(".sectionModal__cadastroProduto");
+
+      modal.classList.remove("sectionModal__cadastroProduto");
     }
+  }
+  static observerCadastro() {
+    this.modalCadastro.addEventListener("click", this.fecharJanela);
+  }
 
-    static showModalCadastro(event) {
-
-        if (event.target.id === "novo-produto") {
-
-            console.log(event.target)
-            const modal = document.querySelector(".sectionModal__cadastroProduto") 
-
-            modal.classList.remove("sectionModal__cadastroProduto")
-            
-        }
+  static fecharJanela(event) {
+    const sectionModal = document.querySelector(".sectionModal");
+    if (event.target.className == "section__fecharmodal") {
+      sectionModal.classList.add("sectionModal__cadastroProduto");
     }
+  }
+  //   static pegarDadosCadastro(event) {
+  //     event.preventDefault();
+  //     const inputs = event.target;
 
-    /* static closeModalCadastro() {
-        this.closeModal.addEventListener("click", this.closeModalCad)
+  //     const dadosCadastro = {};
+
+  //     for (let i = 0; i < inputs.length; i++) {
+  //       if (inputs[i].name) {
+  //         dadosCadastro[inputs[i].name] = inputs[i].value;
+  //       }
+  //     }
+  //     //   window.location.href = "./admin.html";
+  //     //   localStorage.setItem("novoProduto", JSON.stringify(dadosCadastro));
+  //     return dadosCadastro;
+  //   }
+
+  static observerEdit() {
+    this.body.addEventListener("click", this.showEditProduct);
+  }
+
+  static showEditProduct(event) {
+    if (event.target.className === "editProduct") {
+      console.log(event.target);
+      const editModal = document.querySelector(".sectionModal__alterarProduto");
+
+      editModal.classList.remove("sectionModal__alterarProduto");
     }
+  }
 
-    static closeModalCad(event) {
-        const modal = document.querySelector(".sectionModal__cadastroProduto")
-       
-        modal.classList.add("sectionModal__cadastroProduto")
-    } */
-
-    static observerEdit() {
-        this.body.addEventListener("click", this.showEditProduct)
-    }
-
-    static showEditProduct(event) {
-        if (event.target.className === "editProduct") {
-            console.log(event.target)
-            const editModal = document.querySelector(".sectionModal__alterarProduto")
-
-            editModal.classList.remove("sectionModal__alterarProduto")
-        }
-    }
-
-    /*static closeModalEdit() {
+  /*static closeModalEdit() {
         this.modalEdicao.addEventListener("click", this.closeModalEditProduct)
     }
 
@@ -93,6 +106,4 @@ export class ControllerAdmin {
         }
         console.log(newObject)
     } */
-
 }
-
